14a6eaa98942222024bfe2c7c84f59a0d692490c
Marvin Leiers <marvin@leiers.org>
2022-03-03 13:56:30 +0100

    Project setup

---
+++ src/main/java/net/mplugins/bindkeys/BindKeys.java
@@ -0,0 +1,11 @@
+package net.mplugins.bindkeys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BindKeys extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    }
+}
