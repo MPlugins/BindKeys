5c89fbd548674dc2e6fe9b0578fa2604b25b3b6f
Marvin Leiers <marvin@leiers.org>
2022-03-03 14:16:22 +0100

    Created config headers
    Created default config headers and set them on every server start.

--- src/main/java/net/mplugins/bindkeys/BindKeys.java
+++ src/main/java/net/mplugins/bindkeys/BindKeys.java
@@ -2,10 +2,37 @@ package net.mplugins.bindkeys;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class BindKeys extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
+        this.setUpConfigurationFile();
+    }
+
+    private void setUpConfigurationFile()
+    {
+        saveDefaultConfig();
+        getConfig().options().setHeader(getHeaderComments());
+        saveConfig();
+    }
+
+    private List<String> getHeaderComments()
+    {
+        String version = getDescription().getVersion();
+        List<String> header = new ArrayList<>();
+
+        header.add("## ====================== ##");
+        header.add("## Plugin by MPlugins.net ##");
+        header.add("## ====================== ##");
+        header.add(null);
+        header.add("URL: https://github.com/MPlugins/BindKeys");
+        header.add(null);
+        header.add("Version: " + version);
+
+        return header;
     }
 }
